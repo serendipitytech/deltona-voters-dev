f6643bbfff238c0b0be0270449894dd4ad7550f2
serendipitytech <troy@serendipitytech.net>
2023-12-31 20:28:29 -0500

    Update deltona_voters.py

--- deltona_voters.py
+++ deltona_voters.py
@@ -36,44 +36,46 @@ def summarize_voting_data(df, selected_elections, selected_voter_status, selecte
     if selected_party:
         df = df[df['Party'].isin(selected_party)]
     
-    if 'Voting History' not in df.columns:
-        selected_columns = ["03-07-2023 Flagler Beach(Mar/07/2023)", "03/07/2023 Flagler Beach(Mar/07/2023)", "11-08-2022 General Election(Nov/08/2022)", "08-23-2022 Primary Election(Aug/23/2022)", "2022 City of Flagler Beach Election(Mar/08/2022)", "11-02-2021 Municipal Election(Nov/02/2021)", "Daytona Beach Special Primary(Sep/21/2021)", "Municipal Election(Aug/17/2021)", "04-13-2021 Port Orange Primary(Apr/13/2021)", "City of Flagler Beach(Mar/02/2021)", "20201103 General Election(Nov/03/2020)", "20200818 Primary Election(Aug/18/2020)", "20200519 Pierson Mail Ballot Elec(May/19/2020)", "20200317 Pres Preference Primary(Mar/17/2020)", "City of Flagler Beach(Mar/17/2020)", "20191105 Lake Helen General(Nov/05/2019)", "20190611 Pt Orange Special Runoff(Jun/11/2019)", "20190521 Mail Ballot Election(May/21/2019)", "20190430 Pt Orange Special Primary(Apr/30/2019)", "20190402 Edgewater Special General(Apr/02/2019)"]
-        df_voting_history = df[selected_columns].applymap(lambda x: 1 if x in ['Y', 'Z', 'A', 'E', 'F'] else 0)
-        voting_history = df_voting_history[selected_elections].sum(axis=1)
-        df['Voting History'] = voting_history
+    summary_age = df.groupby(['Race', 'Sex', 'Age Range']).size().unstack(fill_value=0)
+    race_order = ["African American", "Hispanic", "White", "Other"]
+    sex_order = ["M", "F", "U"]
+    summary_age = summary_age.reindex(race_order, level='Race')
+    summary_age = summary_age.reindex(sex_order, level='Sex')
+    summary_age = summary_age.reindex(sex_order, level='Sex')
+    summary_age.index = summary_age.index.map(lambda x: f'{x[0]}, {sex_mapping[x[1]]}')  # Combine the multi-index levels into a single string
 
-    # Calculate summary for all parties
-    all_party_summary = df.groupby(['Race', 'Sex', 'Voting History', 'Party']).size().unstack(fill_value=0)
+    row_totals_age = summary_age.sum(axis=1)
+    column_totals_age = summary_age.sum(axis=0)
 
-    # Create a custom index combining race, sex, and number of elections
-    all_party_summary.index = all_party_summary.index.map(
-        lambda x, num_elections=len(selected_elections): f'{x[0]}, {x[1]}, {x[2]} of last {num_elections} elections'
-    )
+    selected_columns = ["03-07-2023 Flagler Beach(Mar/07/2023)", "03/07/2023 Flagler Beach(Mar/07/2023)", "11-08-2022 General Election(Nov/08/2022)", "08-23-2022 Primary Election(Aug/23/2022)", "2022 City of Flagler Beach Election(Mar/08/2022)", "11-02-2021 Municipal Election(Nov/02/2021)", "Daytona Beach Special Primary(Sep/21/2021)", "Municipal Election(Aug/17/2021)", "04-13-2021 Port Orange Primary(Apr/13/2021)", "City of Flagler Beach(Mar/02/2021)", "20201103 General Election(Nov/03/2020)", "20200818 Primary Election(Aug/18/2020)", "20200519 Pierson Mail Ballot Elec(May/19/2020)", "20200317 Pres Preference Primary(Mar/17/2020)", "City of Flagler Beach(Mar/17/2020)", "20191105 Lake Helen General(Nov/05/2019)", "20190611 Pt Orange Special Runoff(Jun/11/2019)", "20190521 Mail Ballot Election(May/21/2019)", "20190430 Pt Orange Special Primary(Apr/30/2019)", "20190402 Edgewater Special General(Apr/02/2019)"]
+    df_voting_history = df[selected_columns].applymap(lambda x: 1 if x in ['Y', 'Z', 'A', 'E', 'F'] else 0)
+    voting_history = df_voting_history[selected_elections].sum(axis=1)
+    df['Voting History'] = voting_history
 
-    # If a party is selected, filter the summary for the selected party
-    if selected_party:
-        selected_party_summary = all_party_summary[selected_party]
-        selected_party_summary['All Parties'] = all_party_summary.sum(axis=1)
-        num_elections = len(selected_elections)
-        selected_party_summary.columns = [f'{i} of {num_elections}' for i in range(num_elections + 1)]
-    else:
-        selected_party_summary = all_party_summary
+    summary_voting_history = df.groupby(['Race', 'Sex', 'Voting History']).size().unstack(fill_value=0)
+    summary_voting_history = summary_voting_history.reindex(race_order, level='Race')
+    summary_voting_history = summary_voting_history.reindex(sex_order, level='Sex')
+    summary_voting_history.index = summary_voting_history.index.map(lambda x: f'{x[0]}, {sex_mapping[x[1]]}')  # Combine the multi-index levels into a single string
 
     num_elections = len(selected_elections)
 
-    selected_party_summary.columns = [f'{i} of {num_elections}' for i in range(num_elections + 1)]
+    summary_voting_history.columns = [f'{i} of {num_elections}' for i in range(num_elections + 1)]
 
-    row_totals_voting_history = selected_party_summary.sum(axis=1)
-    column_totals_voting_history = selected_party_summary.sum(axis=0)
+    # Calculate summary for all parties
+    summary_party_history = df.groupby(['Race', 'Sex', 'Voting History', 'Party']).size().unstack(fill_value=0)
+
+    # Create a custom index combining race, sex, and number of elections
+    summary_party_history.index = summary_party_history.index.map(lambda x, num_elections=num_elections: f'{x[0]} {x[1]}, {x[2]} of last {num_elections} elections')
+
+    summary_voting_history.columns = [f'{i} of {num_elections}' for i in range(num_elections + 1)]
+
+    row_totals_voting_history = summary_voting_history.sum(axis=1)
+    column_totals_voting_history = summary_voting_history.sum(axis=0)
 
     columns_for_detailed_age = ["VoterID", "Race", "Sex", "Birth_Date", "Precinct"]
     columns_for_detailed_voting_history = ["VoterID", "Race", "Sex", "Birth_Date", "Precinct"] + selected_elections
 
-    return (
-        summary_age, row_totals_age, column_totals_age, df[columns_for_detailed_age],
-        selected_party_summary, row_totals_voting_history, column_totals_voting_history, df[columns_for_detailed_voting_history],
-        selected_party_summary  # This includes the summary for all parties or the selected party
-    )
+    return summary_age, row_totals_age, column_totals_age, df[columns_for_detailed_age], summary_voting_history, row_totals_voting_history, column_totals_voting_history, df[columns_for_detailed_voting_history], summary_party_history
 
 def load_data():
     df = pd.read_csv('https://serendipitytech.s3.amazonaws.com/deltona/deltona_voters_streamlit.txt', delimiter=',', low_memory=False)
